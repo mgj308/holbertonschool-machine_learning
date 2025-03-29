9f55b2fb3442c8440014d470812d972321463795
mgj308 <10848@holbertonstudents.com>
2025-03-29 15:02:53 +0100

    1-neuron.py

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+'''Module that has class Neuron'''
+import numpy as np
+
+
+class Neuron:
+    '''Class Neuron that defines a single neuron performing binary class'''
+    def __init__(self, nx):
+        '''Class constructor'''
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        '''Getter function of W'''
+        return self.__W
+
+    @property
+    def b(self):
+        '''Getter function of b'''
+        return self.__b
+
+    @property
+    def A(self):
+        '''Getter function of A'''
+        return self.__A
